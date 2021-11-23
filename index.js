1a143972ac9006f14075a1f757921a8414f5f3f3
LaloJami <eduardolalojami@gmail.com>
2021-11-23 17:15:39 -0500

    [add] POST metodo para crear

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const routerApi = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hola, mi server en express');
 })
@@ -13,6 +15,6 @@ app.get('/nueva-ruta', (req, res) => {
 routerApi(app);
 
 app.listen(port, () => {
-  console.log('Mi post' + port);
+  console.log('Mi port ' + port);
 });
 

3059f8e774bbe68016f7e5390c77999c635b6530
LaloJami <eduardolalojami@gmail.com>
2021-11-23 18:37:20 -0500

    [add] Middleware para HttpErrors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routerApi = require('./routes');
+const {logErrors, errorHandler} = require('./middlewares/error.handler');
 const app = express();
 const port = 3000;
 
@@ -13,6 +14,9 @@ app.get('/nueva-ruta', (req, res) => {
 })
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
